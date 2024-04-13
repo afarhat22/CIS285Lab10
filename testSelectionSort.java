cb09be6f14b039845dc7c647d92c4e4e71ce9501
afarhat22 <112431927+afarhat22@users.noreply.github.com>
2024-04-13 14:10:50 -0400

    Changed to only test mixed and duplicates

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -7,38 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 class testSelectionSort {
-
-	@Test
-	void testPositives() {
-		//Arrange
-		int testArray [] = {45, 13, 56, 75, 34, 23, 1, 67};
-		int expectedOutput [] = {1,13,23,34,45,56,67,75};
-		
-		//act
-		SelectionSort sorter = new SelectionSort();
-		int resultArray [] = sorter.basicSelectionSort(testArray);
-		
-		//assert
-		System.out.println("Positives Result: " + Arrays.toString(resultArray));
-		assertArrayEquals(expectedOutput, resultArray);
-		
-	}
-	
-	@Test
-	void testNegatives() {
-		//Arrange
-		int testArray [] = {-12, -4, -27, -5, -8, -9};
-		int expectedOutput [] = {-27, -12, -9, -8, -5, -4};
-		
-		//act
-		SelectionSort sorter = new SelectionSort();
-		int resultArray [] = sorter.basicSelectionSort(testArray);
-		
-		//assert
-		System.out.println("Negatives Result: " + Arrays.toString(resultArray));
-		assertArrayEquals(expectedOutput, resultArray);
-		
-	}
 	
 	@Test
 	void testMixed() {
