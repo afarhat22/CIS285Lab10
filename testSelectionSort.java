d1a6ed363892604c9ab1ba8463eff6508d8587bc
afarhat22 <112431927+afarhat22@users.noreply.github.com>
2024-04-13 14:07:00 -0400

    Changed to only test Positives and Negatives

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -39,36 +39,4 @@ class testSelectionSort {
 		assertArrayEquals(expectedOutput, resultArray);
 		
 	}
-	
-	@Test
-	void testMixed() {
-		//Arrange
-		int testArray [] = {5, 7, 0, -2, 6, -8};
-		int expectedOutput [] = {-8, -2, 0, 5, 6, 7};
-		
-		//act
-		SelectionSort sorter = new SelectionSort();
-		int resultArray [] = sorter.basicSelectionSort(testArray);
-		
-		//assert
-		System.out.println("Mixed Result: " + Arrays.toString(resultArray));
-		assertArrayEquals(expectedOutput, resultArray);
-		
-	}
-	
-	@Test
-	void testDuplicates() {
-		//Arrange
-		int testArray [] = {7, -4, 5, 7, 5, -2, 5};
-		int expectedOutput [] = {-4, -2, 5, 5, 5, 7, 7};
-		
-		//act
-		SelectionSort sorter = new SelectionSort();
-		int resultArray [] = sorter.basicSelectionSort(testArray);
-		
-		//assert
-		System.out.println("Duplicates Result: " + Arrays.toString(resultArray));
-		assertArrayEquals(expectedOutput, resultArray);
-		
-	}
 }
